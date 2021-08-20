ca8eee9cd93bd4c4ee759c87541c735a8ff85dbc
maariyadiminsky <mariya.github@gmail.com>
2021-08-20 13:39:57 -0600

    setup redux and redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import './index.css';
 
+import App from "./components/App";
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
