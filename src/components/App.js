f4345e08f2dff994c357dc25c54b4dfeecef4db9
maariyadiminsky <mariya.github@gmail.com>
2021-08-20 13:42:18 -0600

    setting up routes wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app">
-      App
+    <div>
+      <BrowserRouter>
+        {/* <Route path="/" exact component={StreamList} /> */}
+      </BrowserRouter>
     </div>
   );
 }
