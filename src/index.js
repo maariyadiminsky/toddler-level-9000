e7a3286e2026e9839d9c7697b62d64c7a66fbfb9
maariyadiminsky <mariya.github@gmail.com>
2021-08-20 22:49:29 -0600

    plugged in Auth0Provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import './index.css';
 
 import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <Auth0Provider
+        domain= {process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId= {process.env.REACT_APP_AUTH0_CLIENT_ID}
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
   </Provider>,
   document.getElementById("root")
 );
