863c924679f41689e12f760be6db16d4c6767f19
maariyadiminsky <mariya.github@gmail.com>
2021-08-25 12:14:14 -0600

    return welcome and start audio on game start

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,6 +1,75 @@
+import LetsPlayAGame_Girl from "../components/Games/audio/LetsPlayAGame_Girl.mp3";
+import LetsPlayAGame_Boy from "../components/Games/audio/LetsPlayAGame_Boy.mp3";
+import LetsPlayAGame_Man from "../components/Games/audio/LetsPlayAGame_Man.mp3";
+import LetsLearnSomeSocialWords_Man from "../components/Games/audio/LetsLearnSomeSocialWords_Man.mp3";
+import LetsLearnSomeSocialWords_Girl from "../components/Games/audio/LetsLearnSomeSocialWords_Girl.mp3";
+
+import WhatAnimalDoYouSee_Girl from "../components/Games/audio/WhatAnimalDoYouSee_Girl.mp3";
+import WhatColorDoYouSee_Boy from "../components/Games/audio/WhatColorDoYouSee_Boy.mp3";
+import WhatFoodItemDoYouSee_Man from "../components/Games/audio/WhatFoodItemDoYouSee_Man.mp3";
+import WhatNumberIsThat_Man from "../components/Games/audio/WhatNumberIsThat_Man.mp3";
+
+import GoodJob_Boy from "../components/Games/audio/GoodJob_Boy.mp3";
+import HoorayYouGotThemAll_Girl from "../components/Games/audio/HoorayYouGotThemAll_Girl.mp3";
+import NiceJob_Girl from "../components/Games/audio/NiceJob_Girl.mp3";
+import NiceJob_Man from "../components/Games/audio/NiceJob_Man.mp3";
+import WowNiceJob_Boy from "../components/Games/audio/WowNiceJob_Boy.mp3";
+import WowNiceJob_Girl from "../components/Games/audio/WowNiceJob_Girl.mp3";
+import WowYouGotThemAll_Man from "../components/Games/audio/WowYouGotThemAll_Man.mp3";
+import YayYouDidIt_Girl from "../components/Games/audio/YayYouDidIt_Girl.mp3";
+import YesYouGotThemAll_Boy from "../components/Games/audio/YesYouGotThemAll_Boy.mp3";
+import YouDidIt_Boy from "../components/Games/audio/YouDidIt_Boy.mp3";
+import YouDidIt_Man from "../components/Games/audio/YouDidIt_Man.mp3";
+
 import {
+    COLOR_TYPE,
+    ANIMAL_TYPE,
+    NUMBER_TYPE,
+    FOOD_TYPE,
     SOCIAL_TYPE,
+    BODY_TYPE
 } from "../const";
+
+let welcomeAudio;
+let startAudio;
+export const getGameStartAudio = (wordType) => {
+    welcomeAudio = "";
+    startAudio = "";
+
+    switch(wordType) {
+        case COLOR_TYPE:
+            welcomeAudio = LetsPlayAGame_Boy;
+            startAudio = WhatColorDoYouSee_Boy;
+            break;
+        case ANIMAL_TYPE:
+            welcomeAudio = LetsPlayAGame_Girl;
+            startAudio = WhatAnimalDoYouSee_Girl;
+            break;
+        case NUMBER_TYPE:
+            welcomeAudio = LetsPlayAGame_Man;
+            startAudio = WhatNumberIsThat_Man;
+            break;
+        case FOOD_TYPE:
+            welcomeAudio = LetsPlayAGame_Man;
+            startAudio = WhatFoodItemDoYouSee_Man;
+            break;
+        case SOCIAL_TYPE:
+            welcomeAudio = LetsPlayAGame_Girl;
+            startAudio = LetsLearnSomeSocialWords_Girl;
+            break;
+        case BODY_TYPE:
+            // todo add here when this game type is ready
+            break;
+            // todo add match time audio here as well.
+        default:
+            break;
+    }
+
+    return {
+        welcomeAudio,
+        startAudio
+    };
+}
 
 export const getCorrectAudioUrl = (sound, wordType) => {
     switch(wordType) {
